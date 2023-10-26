a07b51872371fbc028a07c6b47ad535cbe8a3dc1
Levina <annaLevinaM@yandex.ru>
2023-10-26 23:17:32 +0300

    sprint3 functions

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,12 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(pow: Int): (a: Int) -> Int {
+        return { a: Int -> a.toDouble().pow(pow).toInt() }
     }
 }
