7c23b19bf7001d89da99738cfb6d6c6d8d66803f
Levina <annaLevinaM@yandex.ru>
2023-10-30 21:01:45 +0300

    sprint3 generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,33 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <F : Comparable<F>, S : Comparable<S>> compare(p1: Pair<F, S>, p2: Pair<F, S>): Boolean {
+    return p1.first == p2.first && p1.second == p2.second
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.count { it > elem }
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
+    private val list: MutableList<T> = mutableListOf()
+
+    fun push(elem: T): Boolean = list.add(elem)
+
+    fun pop(): T = list.removeLast()
+
+    fun isEmpty(): Boolean = list.isEmpty()
 
 }
